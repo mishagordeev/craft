b265cf57d88c56e8b42c7d771d6356fe9994415f
mishagordeev <42177098+mishagordeev@users.noreply.github.com>
2022-12-19 22:25:54 +0300

    Init

---
+++ main.js
@@ -0,0 +1,31 @@
+var header = document.querySelector('header')
+var section = document.querySelector('section')
+
+var requestURL = './jsons/items.json'
+var request = new XMLHttpRequest()
+request.open('GET', requestURL)
+request.responseType = 'json'
+request.send();
+request.onload = function() {
+    var items = request.response;
+    drawCards(items,'')
+}
+
+function drawCards(jsonObj, string) {
+    for (var i=0;i<=jsonObj.length;i++) {
+        var itemName = jsonObj[i]['item_name']
+        if (itemName.toLowerCase().includes(string.toLowerCase()) || string.length == 0) {
+            var card = document.createElement('div')
+            var image = document.createElement('img')
+            var name = document.createElement('p')
+            image.src = './images/' + jsonObj[i]['file_name'] + '.png'
+            image.width = 64
+            card.appendChild(image)
+            name.textContent = itemName
+            card.classList.add('card')
+            card.appendChild(name)
+            section.appendChild(card)
+        }
+    }
+}
+
